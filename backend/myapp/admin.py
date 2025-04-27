295f77dc2dc3293604a7c494f0627c3a9b6dfb0f
quytra25 <98822595+quytra25@users.noreply.github.com>
2025-04-27 23:05:57 +0100

    Update admin.py

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Event, Participant, Availability
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email','password')}),
+        ('Personal info', {'fields': ('first_name','last_name','is_registered')}),
+        ('Permissions', {'fields': ('is_active','is_staff','is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','first_name','last_name','is_registered','password1','password2'),
+        }),
+    )
+    list_display = ('email','first_name','last_name','is_registered','is_staff')
+    search_fields = ('email','first_name','last_name')
+    ordering = ('email',)
+
+admin.site.register(Event)
+admin.site.register(Participant)
+admin.site.register(Availability)
