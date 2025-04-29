42a29362397b0864e57ab433b1db860d3aa0c391
quytra25 <98822595+quytra25@users.noreply.github.com>
2025-04-29 18:26:42 +0100

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './index.css'
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
+
+app.use(Toast)
 
 createApp(App).use(router).mount('#app')
