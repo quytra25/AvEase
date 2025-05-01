17e211e97c858fb3acff824be0515b2b9df45229
quytra25 <98822595+quytra25@users.noreply.github.com>
2025-05-02 00:52:37 +0100

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './index.css'
 import Toast from "vue-toastification"
 import "vue-toastification/dist/index.css"
+import { fetchCurrentUser } from '@/composables/useAuth.js'
+import 'bulma/css/bulma.css'
+import './index.css'
 
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
 app.use(Toast)
 
-createApp(App).use(router).mount('#app')
+fetchCurrentUser()
