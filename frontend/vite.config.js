184253be85439d3b4b518ba32e9d850ecb8d7e5e
quytra25 <98822595+quytra25@users.noreply.github.com>
2025-05-02 00:51:41 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // allow @ to map to /src
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     proxy: {
       '/api': {
@@ -12,4 +20,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
