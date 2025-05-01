c3674a1307f199b5626a785c513523f7111bc78f
quytra25 <98822595+quytra25@users.noreply.github.com>
2025-05-02 00:53:53 +0100

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="p-4 border-b flex space-x-4">
+        <router-link to="/">Home - Create Event</router-link>
+        <router-link v-if="isAuthenticated" to="/events">All Events</router-link>
+        <router-link v-if="!isAuthenticated" to="/login">Login</router-link>
+    </nav>
+    <router-view/>
+</template>
+
+<script setup>
+import { useAuth } from '@/composables/useAuth'
+const { isAuthenticated } = useAuth()
+</script>
+
+<style>
+</style>
